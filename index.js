e57d63bbcd855dc2298e09c4f473eb71c3eb4a4a
Genius-Deva <ahmadolanrewaju01@gmail.com>
2025-10-23 22:01:55 +0100

    files updated

---
+++ index.js
@@ -0,0 +1,12 @@
+// let menuNav=document.getElementById('menuNav')
+// let menu=document.getElementById('menu')
+// let nav=document.getElementById('nav')
+// function displayMenu(){
+//   menuNav.style.display='block'
+//   menu.style.display='none'
+
+// }
+// if(nav.style.display=='block'){
+  
+  
+// }
